cc833e910021009de5bab97d4d009165e5851944
riarizkinaa <riarizkina20@gmail.com>
2022-07-24 13:18:32 +0800

    provil pencaker

---
+++ app/Views/layout/template2.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="description" content="">
+  <meta name="keywords" content="">
+  <title>Typography / Universe Admin</title>
+  <link rel="shortcut icon" href="img/favicon.png">
+
+  
+    <link rel="stylesheet" href="fonts/open-sans/style.min.css"> <!-- common font  styles  -->
+<link rel="stylesheet" href="fonts/universe-admin/style.css"> <!-- universeadmin icon font styles -->
+<link rel="stylesheet" href="fonts/mdi/css/materialdesignicons.min.css"> <!-- meterialdesignicons -->
+<link rel="stylesheet" href="fonts/iconfont/style.css"> <!-- DEPRECATED iconmonstr -->
+<link rel="stylesheet" href="vendor/flatpickr/flatpickr.min.css"> <!-- original flatpickr plugin (datepicker) styles -->
+<link rel="stylesheet" href="vendor/simplebar/simplebar.css"> <!-- original simplebar plugin (scrollbar) styles  -->
+<link rel="stylesheet" href="vendor/tagify/tagify.css"> <!-- styles for tags -->
+<link rel="stylesheet" href="vendor/tippyjs/tippy.css"> <!-- original tippy plugin (tooltip) styles -->
+<link rel="stylesheet" href="vendor/select2/css/select2.min.css"> <!-- original select2 plugin styles -->
+<link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css"> <!-- original bootstrap styles -->
+<link rel="stylesheet" href="css/style.min.css" id="stylesheet"> <!-- universeadmin styles -->
+
+  
+
+  <script src="js/ie.assign.fix.min.js"></script>
+</head>
+<body class="js-loading "> <!-- add for rounded corners: form-controls-rounded -->
+
+
+
+<div class="page-preloader js-page-preloader">
+  
+  <div class="page-preloader__desc">Pro Edition</div>
+  <div class="page-preloader__loader">
+    <div class="page-preloader__loader-heading">System Loading</div>
+    <div class="page-preloader__loader-desc">Widgets update</div>
+    <div class="progress progress-rounded page-preloader__loader-progress">
+      <div id="page-loader-progress-bar" class="progress-bar bg-info" role="progressbar" style="width: 10%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+    </div>
+  </div>
+  <div class="page-preloader__copyright">ThemesAnytime, 2018</div>
+</div>
+
+
+
+  <div class="navbar navbar-light navbar-expand-lg">
+  <button class="sidebar-toggler" type="button">
+    <span class="ua-icon-sidebar-open sidebar-toggler__open"></span>
+    <span class="ua-icon-alert-close sidebar-toggler__close"></span>
+  </button>
+
+  <span class="navbar-brand">
+    <a href="/"><img src="img/logo.png" alt="" class="navbar-brand__logo"></a>
+    <span class="ua-icon-menu slide-nav-toggle"></span>
+  </span>
+
+  <span class="navbar-brand-sm">
+    <a href="/"><img src="img/logo-sm.png" alt="" class="navbar-brand__logo"></a>
+    <span class="ua-icon-menu slide-nav-toggle"></span>
+  </span>
+
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-collapse">
+    <span class="ua-icon-navbar-open navbar-toggler__open"></span>
+    <span class="ua-icon-alert-close navbar-toggler__close"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbar-collapse">
+    <div class="navbar__menu">
+      <ul class="navbar-nav">
+        <li class="nav-item ">
+          <a class="nav-link">
+            Home
+          </a>
+          
+        </li>
+        <li class="nav-item dropdown dropdown__columns navbar__menu-item">
+          <a class="nav-link ">
+            info Lowongan
+          </a>
+        
+        </li>
+        <li class="nav-item navbar__menu-item">
+          <a class="nav-link" href="#">Perusahaan</a>
+        </li>
+      </ul>
+      
+
+    </div>
+    </body>
+</html>
