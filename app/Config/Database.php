5f7417029d4119a5e5265b40b3c676118b97f650
riarizkinaa <riarizkina20@gmail.com>
2022-07-27 19:32:52 +0800

    not yet

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'praktikum10',
+        'database' => 'jobs',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
