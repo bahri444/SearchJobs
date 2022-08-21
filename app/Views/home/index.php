d35712b9e7da32da8d0a7b0a4fc6d7342ac33090
bahri444 <bahrysaipul266@gmail.com>
2022-08-21 13:05:38 +0800

    done index

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -7,24 +7,18 @@
   <h1 class="text-center">Semua Lowongan Pekerjaan</h1>
 </div>
 <div class="row ml-2 mr-2">
-  <div class="col-4">
-    <div class="main-container">
-      <h3>Hierarchy of Type</h3>
+  <?php foreach ($loker as $row) : ?>
+    <div class="col-4">
+      <div class="main-container">
+        <h3 class="text-center"><b><?= $row['judul_loker'] ?></b></h3>
+        <p><b>Nama Kategori :</b><?= $row['nm_ktgr'] ?></p>
+        <p><b>Nama Perusahaan :</b><?= $row['nm_prshn'] ?></p>
+        <p><b>Posisi :</b><?= $row['posisi'] ?></p>
+        <p><b>Tgl Buka :</b><?= $row['tgl_buka'] ?></p>
+        <p><b>Tgl Tutup :</b><?= $row['tgl_tutup'] ?></p>
+        <p><b>Di Posting :</b><?= $row['created_at'] ?></p>
+      </div>
     </div>
-  </div>
-  <div class="col-4">
-    <div class="main-container">
-      <h3>Basic Paragraph
-        <span>14px</span>
-      </h3>
-    </div>
-  </div>
-  <div class="col-4">
-    <div class="main-container">
-      <h3>Basic Paragraph
-        <span>14px</span>
-      </h3>
-    </div>
-  </div>
+  <?php endforeach; ?>
 </div>
 <?= $this->endSection(); ?>
